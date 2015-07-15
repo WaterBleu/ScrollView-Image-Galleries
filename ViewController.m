0aa7e5f0723fe65a38d6099d843786a1bac360f3
WaterBleu <jeff7746@hotmail.com>
2015-07-14 18:57:50 -0700

    Complete part III

--- ViewController.m
+++ ViewController.m
@@ -7,9 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "ImageDetailViewController.h"
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UIScrollView *uiScrollerView;
+@property (nonatomic) UITapGestureRecognizer *tapGesture;
+@property (nonatomic) CGPoint tapPoint;
+
+@property (nonatomic) UIImageView *imagePlaceholder1;
+@property (nonatomic) UIImageView *imagePlaceholder2;
+@property (nonatomic) UIImageView *imagePlaceholder3;
 
 @end
 
@@ -19,88 +27,92 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    UIImage *img = [UIImage imageNamed:@"Lighthouse-in-Field"];
-    UIImageView *imagePlaceholder1 = [[UIImageView alloc] initWithImage:img];
-    [imagePlaceholder1 setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.uiScrollerView addSubview:imagePlaceholder1];
-    [self setViewConstraintWithItem:imagePlaceholder1
+    self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapImage:)];
+    
+    [self.uiScrollerView addGestureRecognizer:self.tapGesture];
+    
+    UIImage *img1 = [UIImage imageNamed:@"Lighthouse-in-Field"];
+    UIImage *img2 = [UIImage imageNamed:@"Lighthouse-night"];
+    UIImage *img3 = [UIImage imageNamed:@"Lighthouse-zoomed"];
+
+    self.imagePlaceholder1 = [[UIImageView alloc] initWithImage:img1];
+    [self.imagePlaceholder1 setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.uiScrollerView addSubview:self.imagePlaceholder1];
+    [self setViewConstraintWithItem:self.imagePlaceholder1
                    andWithAttribute:NSLayoutAttributeLeft
                           andToItem:self.uiScrollerView
                    andWithAttribute:NSLayoutAttributeLeft
                   andWithMultiplier:1
                     andWithConstant:0];
-    [self setViewConstraintWithItem:imagePlaceholder1
+    [self setViewConstraintWithItem:self.imagePlaceholder1
                    andWithAttribute:NSLayoutAttributeTopMargin
                           andToItem:self.uiScrollerView
                    andWithAttribute:NSLayoutAttributeTopMargin
                   andWithMultiplier:1
                     andWithConstant:0];
-    [self setViewConstraintWithItem:imagePlaceholder1
+    [self setViewConstraintWithItem:self.imagePlaceholder1
                    andWithAttribute:NSLayoutAttributeHeight
                           andToItem:nil
                    andWithAttribute:NSLayoutAttributeHeight
                   andWithMultiplier:1
                     andWithConstant:[UIScreen mainScreen].bounds.size.height];
-    [self setViewConstraintWithItem:imagePlaceholder1
+    [self setViewConstraintWithItem:self.imagePlaceholder1
                    andWithAttribute:NSLayoutAttributeWidth
                           andToItem:nil
                    andWithAttribute:NSLayoutAttributeWidth
                   andWithMultiplier:1
                     andWithConstant:[UIScreen mainScreen].bounds.size.width];
     
-    img = [UIImage imageNamed:@"Lighthouse-night"];
-    UIImageView *imagePlaceholder2 = [[UIImageView alloc] initWithImage:img];
-    [imagePlaceholder2 setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.uiScrollerView addSubview:imagePlaceholder2];
-    [self setViewConstraintWithItem:imagePlaceholder2
+    self.imagePlaceholder2 = [[UIImageView alloc] initWithImage:img2];
+    [self.imagePlaceholder2 setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.uiScrollerView addSubview:self.imagePlaceholder2];
+    [self setViewConstraintWithItem:self.imagePlaceholder2
                    andWithAttribute:NSLayoutAttributeLeading
-                          andToItem:imagePlaceholder1
+                          andToItem:self.imagePlaceholder1
                    andWithAttribute:NSLayoutAttributeTrailing
                   andWithMultiplier:1
                     andWithConstant:0];
-    [self setViewConstraintWithItem:imagePlaceholder2
+    [self setViewConstraintWithItem:self.imagePlaceholder2
                    andWithAttribute:NSLayoutAttributeTopMargin
                           andToItem:self.uiScrollerView
                    andWithAttribute:NSLayoutAttributeTopMargin
                   andWithMultiplier:1
                     andWithConstant:0];
-    [self setViewConstraintWithItem:imagePlaceholder2
+    [self setViewConstraintWithItem:self.imagePlaceholder2
                    andWithAttribute:NSLayoutAttributeHeight
                           andToItem:nil
                    andWithAttribute:NSLayoutAttributeHeight
                   andWithMultiplier:1
                     andWithConstant:[UIScreen mainScreen].bounds.size.height];
-    [self setViewConstraintWithItem:imagePlaceholder2
+    [self setViewConstraintWithItem:self.imagePlaceholder2
                    andWithAttribute:NSLayoutAttributeWidth
                           andToItem:nil
                    andWithAttribute:NSLayoutAttributeWidth
                   andWithMultiplier:1
                     andWithConstant:[UIScreen mainScreen].bounds.size.width];
 
-    
-    img = [UIImage imageNamed:@"Lighthouse-zoomed"];
-    UIImageView *imagePlaceholder3 = [[UIImageView alloc] initWithImage:img];
-    [imagePlaceholder3 setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self.uiScrollerView addSubview:imagePlaceholder3];
-    [self setViewConstraintWithItem:imagePlaceholder3
+    self.imagePlaceholder3 = [[UIImageView alloc] initWithImage:img3];
+    [self.imagePlaceholder3 setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.uiScrollerView addSubview:self.imagePlaceholder3];
+    [self setViewConstraintWithItem:self.imagePlaceholder3
                    andWithAttribute:NSLayoutAttributeLeading
-                          andToItem:imagePlaceholder2
+                          andToItem:self.imagePlaceholder2
                    andWithAttribute:NSLayoutAttributeTrailing
                   andWithMultiplier:1
                     andWithConstant:0];
-    [self setViewConstraintWithItem:imagePlaceholder3
+    [self setViewConstraintWithItem:self.imagePlaceholder3
                    andWithAttribute:NSLayoutAttributeRight
                           andToItem:self.uiScrollerView
                    andWithAttribute:NSLayoutAttributeRight
                   andWithMultiplier:1
                     andWithConstant:0];
-    [self setViewConstraintWithItem:imagePlaceholder3
+    [self setViewConstraintWithItem:self.imagePlaceholder3
                    andWithAttribute:NSLayoutAttributeHeight
                           andToItem:nil
                    andWithAttribute:NSLayoutAttributeHeight
                   andWithMultiplier:1
                     andWithConstant:[UIScreen mainScreen].bounds.size.height];
-    [self setViewConstraintWithItem:imagePlaceholder3
+    [self setViewConstraintWithItem:self.imagePlaceholder3
                    andWithAttribute:NSLayoutAttributeWidth
                           andToItem:nil
                    andWithAttribute:NSLayoutAttributeWidth
@@ -113,9 +125,6 @@
     
     self.uiScrollerView.pagingEnabled = YES;
     self.uiScrollerView.delegate = self;
-    
-    for(UIView *v in self.uiScrollerView.subviews)
-        NSLog(@"width: %f", v.bounds.size.width);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -131,6 +140,40 @@
     }
     
     targetContentOffset -> x = pageIndex * [UIScreen mainScreen].bounds.size.width;
+}
+
+- (void)tapImage:(id)sender{
+    UITapGestureRecognizer *tapGesture = (UITapGestureRecognizer*)sender;
+    if(tapGesture.state == UIGestureRecognizerStateBegan){
+        self.tapPoint = [tapGesture locationInView:self.uiScrollerView];
+    }
+    if(tapGesture.state == UIGestureRecognizerStateEnded){
+        if([self checkView:self.imagePlaceholder1 withCurrentLocation:self.tapPoint]){
+            [self performSegueWithIdentifier:@"zoomPhoto" sender:self.imagePlaceholder1];
+        }
+        else if([self checkView:self.imagePlaceholder2 withCurrentLocation:self.tapPoint]){
+            [self performSegueWithIdentifier:@"zoomPhoto" sender:self.imagePlaceholder2];
+        }
+        else if([self checkView:self.imagePlaceholder3 withCurrentLocation:self.tapPoint]){
+            [self performSegueWithIdentifier:@"zoomPhoto" sender:self.imagePlaceholder3];
+        }
+    }
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if([segue.identifier isEqualToString:@"zoomPhoto"]){
+        ImageDetailViewController *imgDVC = [segue destinationViewController];
+        UIImageView *tempHolder = sender;
+        imgDVC.zoomView.image = tempHolder.image;
+    }
+}
+
+- (BOOL)checkView:(UIView*) checkedView withCurrentLocation:(CGPoint) currLoc{
+    if (CGRectContainsPoint(checkedView.frame, currLoc)) {
+        return true;
+    }
+    else
+        return false;
 }
 
 - (void)setViewConstraintWithItem:(UIImageView*) imgView
